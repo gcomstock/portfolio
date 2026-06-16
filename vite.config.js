21e3f824955452f640e4ab65c1f4240d10ea1dfe
Greg Comstock <gcomstock@gmail.com>
2026-06-16 14:11:50 -0700

    config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import mdx from '@mdx-js/rollup';
 
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH ?? '/portfolio/',
+  base: process.env.VITE_BASE_PATH ?? '/',
   plugins: [mdx({ providerImportSource: '@mdx-js/react' }), react()],
 });
